c00c72bfad0337a37facaaeba8c46165b650b06e
priyam-hub <palpriyam95@gmail.com>
2023-01-15 10:11:50 +0530

    Add project files.

---
+++ DashBoard.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace PayrollManagementSystem
+{
+    partial class DashBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashBoard));
+            this.Label_Heading = new System.Windows.Forms.Label();
+            this.Button_Home = new System.Windows.Forms.Button();
+            this.Button_Attendance = new System.Windows.Forms.Button();
+            this.Button_Salary = new System.Windows.Forms.Button();
+            this.Button_Employee = new System.Windows.Forms.Button();
+            this.Button_Reports = new System.Windows.Forms.Button();
+            this.Button_Settings = new System.Windows.Forms.Button();
+            this.Button_Exit = new System.Windows.Forms.Button();
+            this.Button_Brance = new System.Windows.Forms.Button();
+            this.Panel_Dashboard = new System.Windows.Forms.Panel();
+            this.Panel_Heading = new System.Windows.Forms.Panel();
+            this.PictureBox_Logo = new System.Windows.Forms.PictureBox();
+            this.Label_Company = new System.Windows.Forms.Label();
+            this.Button_LogOut = new System.Windows.Forms.Button();
+            this.Panel_Dashboard.SuspendLayout();
+            this.Panel_Heading.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label_Heading
+            // 
+            this.Label_Heading.AutoSize = true;
+            this.Label_Heading.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Heading.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Heading.ForeColor = System.Drawing.Color.White;
+            this.Label_Heading.Location = new System.Drawing.Point(344, 62);
+            this.Label_Heading.Name = "Label_Heading";
+            this.Label_Heading.Size = new System.Drawing.Size(445, 40);
+            this.Label_Heading.TabIndex = 1;
+            this.Label_Heading.Text = "Payroll Management System";
+            // 
+            // Button_Home
+            // 
+            this.Button_Home.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Home.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Home.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Home.ForeColor = System.Drawing.Color.White;
+            this.Button_Home.Location = new System.Drawing.Point(385, 187);
+            this.Button_Home.Name = "Button_Home";
+            this.Button_Home.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_Home.Size = new System.Drawing.Size(136, 90);
+            this.Button_Home.TabIndex = 2;
+            this.Button_Home.Text = "Home";
+            this.Button_Home.UseVisualStyleBackColor = false;
+            // 
+            // Button_Attendance
+            // 
+            this.Button_Attendance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Attendance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Attendance.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Attendance.ForeColor = System.Drawing.Color.White;
+            this.Button_Attendance.Location = new System.Drawing.Point(913, 187);
+            this.Button_Attendance.Name = "Button_Attendance";
+            this.Button_Attendance.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_Attendance.Size = new System.Drawing.Size(146, 90);
+            this.Button_Attendance.TabIndex = 4;
+            this.Button_Attendance.Text = "Attendance";
+            this.Button_Attendance.UseVisualStyleBackColor = false;
+            // 
+            // Button_Salary
+            // 
+            this.Button_Salary.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Salary.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Salary.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Salary.ForeColor = System.Drawing.Color.White;
+            this.Button_Salary.Location = new System.Drawing.Point(738, 187);
+            this.Button_Salary.Name = "Button_Salary";
+            this.Button_Salary.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_Salary.Size = new System.Drawing.Size(136, 90);
+            this.Button_Salary.TabIndex = 5;
+            this.Button_Salary.Text = "Salary";
+            this.Button_Salary.UseVisualStyleBackColor = false;
+            // 
+            // Button_Employee
+            // 
+            this.Button_Employee.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Employee.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Employee.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Employee.ForeColor = System.Drawing.Color.White;
+            this.Button_Employee.Location = new System.Drawing.Point(565, 187);
+            this.Button_Employee.Name = "Button_Employee";
+            this.Button_Employee.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_Employee.Size = new System.Drawing.Size(136, 90);
+            this.Button_Employee.TabIndex = 6;
+            this.Button_Employee.Text = "Employee";
+            this.Button_Employee.UseVisualStyleBackColor = false;
+            // 
+            // Button_Reports
+            // 
+            this.Button_Reports.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Reports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Reports.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Reports.ForeColor = System.Drawing.Color.White;
+            this.Button_Reports.Location = new System.Drawing.Point(565, 317);
+            this.Button_Reports.Name = "Button_Reports";
+            this.Button_Reports.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_Reports.Size = new System.Drawing.Size(136, 90);
+            this.Button_Reports.TabIndex = 10;
+            this.Button_Reports.Text = "Reports";
+            this.Button_Reports.UseVisualStyleBackColor = false;
+            // 
+            // Button_Settings
+            // 
+            this.Button_Settings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Settings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Settings.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Settings.ForeColor = System.Drawing.Color.White;
+            this.Button_Settings.Location = new System.Drawing.Point(738, 317);
+            this.Button_Settings.Name = "Button_Settings";
+            this.Button_Settings.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_Settings.Size = new System.Drawing.Size(136, 90);
+            this.Button_Settings.TabIndex = 9;
+            this.Button_Settings.Text = "Settings";
+            this.Button_Settings.UseVisualStyleBackColor = false;
+            // 
+            // Button_Exit
+            // 
+            this.Button_Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Exit.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Exit.ForeColor = System.Drawing.Color.White;
+            this.Button_Exit.Location = new System.Drawing.Point(913, 317);
+            this.Button_Exit.Name = "Button_Exit";
+            this.Button_Exit.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_Exit.Size = new System.Drawing.Size(146, 90);
+            this.Button_Exit.TabIndex = 8;
+            this.Button_Exit.Text = "Exit";
+            this.Button_Exit.UseVisualStyleBackColor = false;
+            this.Button_Exit.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Button_Brance
+            // 
+            this.Button_Brance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Brance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Brance.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Brance.ForeColor = System.Drawing.Color.White;
+            this.Button_Brance.Location = new System.Drawing.Point(385, 317);
+            this.Button_Brance.Name = "Button_Brance";
+            this.Button_Brance.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_Brance.Size = new System.Drawing.Size(136, 90);
+            this.Button_Brance.TabIndex = 7;
+            this.Button_Brance.Text = "Branch";
+            this.Button_Brance.UseVisualStyleBackColor = false;
+            // 
+            // Panel_Dashboard
+            // 
+            this.Panel_Dashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Panel_Dashboard.Controls.Add(this.PictureBox_Logo);
+            this.Panel_Dashboard.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Panel_Dashboard.Location = new System.Drawing.Point(0, 0);
+            this.Panel_Dashboard.Name = "Panel_Dashboard";
+            this.Panel_Dashboard.Size = new System.Drawing.Size(196, 509);
+            this.Panel_Dashboard.TabIndex = 11;
+            // 
+            // Panel_Heading
+            // 
+            this.Panel_Heading.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Panel_Heading.Controls.Add(this.Button_LogOut);
+            this.Panel_Heading.Controls.Add(this.Label_Company);
+            this.Panel_Heading.Controls.Add(this.Label_Heading);
+            this.Panel_Heading.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel_Heading.Location = new System.Drawing.Point(196, 0);
+            this.Panel_Heading.Name = "Panel_Heading";
+            this.Panel_Heading.Size = new System.Drawing.Size(1041, 142);
+            this.Panel_Heading.TabIndex = 12;
+            // 
+            // PictureBox_Logo
+            // 
+            this.PictureBox_Logo.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox_Logo.Image")));
+            this.PictureBox_Logo.Location = new System.Drawing.Point(3, 3);
+            this.PictureBox_Logo.Name = "PictureBox_Logo";
+            this.PictureBox_Logo.Size = new System.Drawing.Size(190, 139);
+            this.PictureBox_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox_Logo.TabIndex = 3;
+            this.PictureBox_Logo.TabStop = false;
+            // 
+            // Label_Company
+            // 
+            this.Label_Company.AutoSize = true;
+            this.Label_Company.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Company.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Company.ForeColor = System.Drawing.Color.White;
+            this.Label_Company.Location = new System.Drawing.Point(319, 9);
+            this.Label_Company.Name = "Label_Company";
+            this.Label_Company.Size = new System.Drawing.Size(492, 40);
+            this.Label_Company.TabIndex = 2;
+            this.Label_Company.Text = "MCKV Institute of Engineering";
+            // 
+            // Button_LogOut
+            // 
+            this.Button_LogOut.BackColor = System.Drawing.Color.White;
+            this.Button_LogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_LogOut.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_LogOut.ForeColor = System.Drawing.Color.Red;
+            this.Button_LogOut.Location = new System.Drawing.Point(893, 12);
+            this.Button_LogOut.Name = "Button_LogOut";
+            this.Button_LogOut.Padding = new System.Windows.Forms.Padding(2);
+            this.Button_LogOut.Size = new System.Drawing.Size(136, 113);
+            this.Button_LogOut.TabIndex = 13;
+            this.Button_LogOut.Text = "Log Out";
+            this.Button_LogOut.UseVisualStyleBackColor = false;
+            this.Button_LogOut.Click += new System.EventHandler(this.Button_LogOut_Click);
+            // 
+            // DashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1237, 509);
+            this.Controls.Add(this.Panel_Heading);
+            this.Controls.Add(this.Panel_Dashboard);
+            this.Controls.Add(this.Button_Reports);
+            this.Controls.Add(this.Button_Settings);
+            this.Controls.Add(this.Button_Exit);
+            this.Controls.Add(this.Button_Brance);
+            this.Controls.Add(this.Button_Employee);
+            this.Controls.Add(this.Button_Salary);
+            this.Controls.Add(this.Button_Attendance);
+            this.Controls.Add(this.Button_Home);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DashBoard";
+            this.Text = "DashBoard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Panel_Dashboard.ResumeLayout(false);
+            this.Panel_Heading.ResumeLayout(false);
+            this.Panel_Heading.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_Heading;
+        private System.Windows.Forms.Button Button_Home;
+        private System.Windows.Forms.Button Button_Attendance;
+        private System.Windows.Forms.Button Button_Salary;
+        private System.Windows.Forms.Button Button_Employee;
+        private System.Windows.Forms.Button Button_Reports;
+        private System.Windows.Forms.Button Button_Settings;
+        private System.Windows.Forms.Button Button_Exit;
+        private System.Windows.Forms.Button Button_Brance;
+        private System.Windows.Forms.Panel Panel_Dashboard;
+        private System.Windows.Forms.Panel Panel_Heading;
+        private System.Windows.Forms.PictureBox PictureBox_Logo;
+        private System.Windows.Forms.Button Button_LogOut;
+        private System.Windows.Forms.Label Label_Company;
+    }
+}
---
+++ Login.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace PayrollManagementSystem
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.Label_Heading = new System.Windows.Forms.Label();
+            this.GroupBox_Login = new System.Windows.Forms.GroupBox();
+            this.PictureBox_Logo = new System.Windows.Forms.PictureBox();
+            this.Label_Username = new System.Windows.Forms.Label();
+            this.Textbox_Username = new System.Windows.Forms.TextBox();
+            this.Textbox_Password = new System.Windows.Forms.TextBox();
+            this.Label_Password = new System.Windows.Forms.Label();
+            this.LinkLabel_ForgetPassword = new System.Windows.Forms.LinkLabel();
+            this.Button_Login = new System.Windows.Forms.Button();
+            this.Button_Cancel = new System.Windows.Forms.Button();
+            this.GroupBox_Login.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label_Heading
+            // 
+            this.Label_Heading.AutoSize = true;
+            this.Label_Heading.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Heading.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Heading.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Label_Heading.Location = new System.Drawing.Point(627, 53);
+            this.Label_Heading.Name = "Label_Heading";
+            this.Label_Heading.Size = new System.Drawing.Size(445, 40);
+            this.Label_Heading.TabIndex = 0;
+            this.Label_Heading.Text = "Payroll Management System";
+            // 
+            // GroupBox_Login
+            // 
+            this.GroupBox_Login.Controls.Add(this.Button_Cancel);
+            this.GroupBox_Login.Controls.Add(this.Button_Login);
+            this.GroupBox_Login.Controls.Add(this.LinkLabel_ForgetPassword);
+            this.GroupBox_Login.Controls.Add(this.Textbox_Password);
+            this.GroupBox_Login.Controls.Add(this.Label_Password);
+            this.GroupBox_Login.Controls.Add(this.Textbox_Username);
+            this.GroupBox_Login.Controls.Add(this.Label_Username);
+            this.GroupBox_Login.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GroupBox_Login.Location = new System.Drawing.Point(611, 110);
+            this.GroupBox_Login.Name = "GroupBox_Login";
+            this.GroupBox_Login.Size = new System.Drawing.Size(476, 381);
+            this.GroupBox_Login.TabIndex = 1;
+            this.GroupBox_Login.TabStop = false;
+            this.GroupBox_Login.Text = "Please Login first";
+            // 
+            // PictureBox_Logo
+            // 
+            this.PictureBox_Logo.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox_Logo.Image")));
+            this.PictureBox_Logo.Location = new System.Drawing.Point(188, 110);
+            this.PictureBox_Logo.Name = "PictureBox_Logo";
+            this.PictureBox_Logo.Size = new System.Drawing.Size(380, 380);
+            this.PictureBox_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.PictureBox_Logo.TabIndex = 2;
+            this.PictureBox_Logo.TabStop = false;
+            // 
+            // Label_Username
+            // 
+            this.Label_Username.AutoSize = true;
+            this.Label_Username.Location = new System.Drawing.Point(58, 77);
+            this.Label_Username.Name = "Label_Username";
+            this.Label_Username.Size = new System.Drawing.Size(119, 23);
+            this.Label_Username.TabIndex = 0;
+            this.Label_Username.Text = "User Name : ";
+            // 
+            // Textbox_Username
+            // 
+            this.Textbox_Username.Location = new System.Drawing.Point(183, 68);
+            this.Textbox_Username.Name = "Textbox_Username";
+            this.Textbox_Username.Size = new System.Drawing.Size(243, 32);
+            this.Textbox_Username.TabIndex = 1;
+            // 
+            // Textbox_Password
+            // 
+            this.Textbox_Password.Location = new System.Drawing.Point(183, 111);
+            this.Textbox_Password.Name = "Textbox_Password";
+            this.Textbox_Password.PasswordChar = '*';
+            this.Textbox_Password.Size = new System.Drawing.Size(243, 32);
+            this.Textbox_Password.TabIndex = 3;
+            // 
+            // Label_Password
+            // 
+            this.Label_Password.AutoSize = true;
+            this.Label_Password.Location = new System.Drawing.Point(71, 120);
+            this.Label_Password.Name = "Label_Password";
+            this.Label_Password.Size = new System.Drawing.Size(106, 23);
+            this.Label_Password.TabIndex = 2;
+            this.Label_Password.Text = "Password : ";
+            // 
+            // LinkLabel_ForgetPassword
+            // 
+            this.LinkLabel_ForgetPassword.AutoSize = true;
+            this.LinkLabel_ForgetPassword.Location = new System.Drawing.Point(228, 146);
+            this.LinkLabel_ForgetPassword.Name = "LinkLabel_ForgetPassword";
+            this.LinkLabel_ForgetPassword.Size = new System.Drawing.Size(160, 23);
+            this.LinkLabel_ForgetPassword.TabIndex = 4;
+            this.LinkLabel_ForgetPassword.TabStop = true;
+            this.LinkLabel_ForgetPassword.Text = "Forget Password?";
+            // 
+            // Button_Login
+            // 
+            this.Button_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Login.Location = new System.Drawing.Point(183, 182);
+            this.Button_Login.Name = "Button_Login";
+            this.Button_Login.Size = new System.Drawing.Size(243, 38);
+            this.Button_Login.TabIndex = 5;
+            this.Button_Login.Text = "Login";
+            this.Button_Login.UseVisualStyleBackColor = false;
+            this.Button_Login.Click += new System.EventHandler(this.Button_Login_Click);
+            // 
+            // Button_Cancel
+            // 
+            this.Button_Cancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Button_Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Cancel.Location = new System.Drawing.Point(183, 226);
+            this.Button_Cancel.Name = "Button_Cancel";
+            this.Button_Cancel.Size = new System.Drawing.Size(243, 38);
+            this.Button_Cancel.TabIndex = 6;
+            this.Button_Cancel.Text = "Cancel";
+            this.Button_Cancel.UseVisualStyleBackColor = false;
+            this.Button_Cancel.Click += new System.EventHandler(this.Button_Cancel_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.Controls.Add(this.PictureBox_Logo);
+            this.Controls.Add(this.GroupBox_Login);
+            this.Controls.Add(this.Label_Heading);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.GroupBox_Login.ResumeLayout(false);
+            this.GroupBox_Login.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_Heading;
+        private System.Windows.Forms.GroupBox GroupBox_Login;
+        private System.Windows.Forms.PictureBox PictureBox_Logo;
+        private System.Windows.Forms.Button Button_Cancel;
+        private System.Windows.Forms.Button Button_Login;
+        private System.Windows.Forms.LinkLabel LinkLabel_ForgetPassword;
+        private System.Windows.Forms.TextBox Textbox_Password;
+        private System.Windows.Forms.Label Label_Password;
+        private System.Windows.Forms.TextBox Textbox_Username;
+        private System.Windows.Forms.Label Label_Username;
+    }
+}
+
